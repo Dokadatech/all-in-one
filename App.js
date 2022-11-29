a2aabb9108b765485a34f93327aefb46f8809cc3
Dokadatech <113148488+Dokadatech@users.noreply.github.com>
2022-11-29 00:02:11 -0400

    change made

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import NavigatorAuth from "./src/navigation/NavigatorAuth";
-import { store } from "./src/redux/store";
+// import { store } from "./src/redux/store";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Provider store={store}>
-        <StatusBar style="light" />
+      {/* <Provider store={store}> */}
+      <StatusBar style="light" />
 
-        <NavigatorAuth />
-      </Provider>
+      <NavigatorAuth />
+      {/* </Provider> */}
     </NavigationContainer>
   );
 }
